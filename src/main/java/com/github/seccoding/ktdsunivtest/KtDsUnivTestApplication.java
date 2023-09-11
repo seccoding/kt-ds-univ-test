a8d58dd78282515cce9cb6cff9e4c5b6e69139c1
DESKTOP-BN355R5\Min Chang Jang <mcjang1116@gmail.com>
2023-09-11 13:13:54 +0900

    sfsafd

--- src/main/java/com/github/seccoding/ktdsunivtest/KtDsUnivTestApplication.java
+++ src/main/java/com/github/seccoding/ktdsunivtest/KtDsUnivTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KtDsUnivTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KtDsUnivTestApplication.class, args);
+		SpringApplication.run(KtDsUnivTestApplication.class, args); 
 	}
 
 }
